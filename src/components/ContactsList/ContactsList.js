f772e431c927b3e5fdea2a398fc3095fe2609f95
Anidamp <79271758+Anidamp@users.noreply.github.com>
2021-12-07 14:11:15 +0200

    fix

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -6,12 +6,13 @@ export default function ContactList() {
   const [delContact] = useDeleteContactMutation();
   const filter = useSelector(state => state.filter);
   const { data } = useGetContactsQuery();
+  console.log(filter)
   return (
   <>
     {data && (
     <ul className={s.list}>
       {data.filter(contact =>
-              contact.name.toLocaleLowerCase().includes(filter.toLowerCase()), console.log(data),
+              contact.name.toLocaleLowerCase().includes(filter.toLowerCase()),
             ).map(({id, name, phone}) =>
         <li key={id} className={s.item}>
           <p>{name}</p>
